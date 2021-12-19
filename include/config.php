12c19ace850c98fb1d11abf86b545ebe5efafee3
yusptar <yusupvatar@gmail.com>
2021-12-19 19:29:45 +0700

    update docker

--- include/config.php
+++ include/config.php
@@ -1,5 +1,5 @@
 <?php
-$host     = "localhost";    // Nama host
+$host     = "172.20.0.2";    // Nama host
 $username = "root";         // Username database
 $password = "";   // Password database
 $database = "ams_native";   // Nama database
